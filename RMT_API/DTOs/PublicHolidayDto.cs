af5dedf0dd1ae5c20ad1ce335413b40a162ca1dd
PrasadImandi <prasadimandi208@gmail.com>
2025-02-06 14:53:27 +0530

    added description in DTO

--- RMT_API/DTOs/PublicHolidayDto.cs
+++ RMT_API/DTOs/PublicHolidayDto.cs
@@ -4,6 +4,7 @@ namespace RMT_API.DTOs
 {
 	public class PublicHolidayDto :BaseDto
 	{
+		public string? Description { get; set; }
 		public bool? IsPublic{get;set; }
 		public DateTime PHDate { get; set; }
 		public DateTime PHYear { get; set; }
