e541650508aa8021cb45e317280db45c6c1e3ee1
PrasadImandi <prasadimandi208@gmail.com>
2025-02-10 10:41:48 +0530

    updated migration changes

--- RMT_API/Migrations/20250205102059_firstmigration_1.Designer.cs
+++ RMT_API/Migrations/20250205102059_firstmigration_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RMT_API.Data;
 
@@ -11,9 +12,11 @@ using RMT_API.Data;
 namespace RMT_API.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250205102059_firstmigration_1")]
+    partial class firstmigration_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
