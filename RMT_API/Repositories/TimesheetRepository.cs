174ed705dea58c1ec4f27b99902ac0c86064f66a
PrasadImandi <prasadimandi208@gmail.com>
2025-02-16 19:29:14 +0530

    Updated total hours in timesheet based on holidays

--- RMT_API/Repositories/TimesheetRepository.cs
+++ RMT_API/Repositories/TimesheetRepository.cs
@@ -18,11 +18,7 @@ namespace RMT_API.Repositories
 			
 			if (weekTimesheet == null)
 			{
-				var holidaysInWeek =await _context.PublicHolidaysMaster
-						.Where(h => h.PHDate >= startOfNextWeek && h.PHDate <= endOfNextWeek && (h.IsActive??false))
-						.ToListAsync();
-
-				int totalHours = 40- (holidaysInWeek.Count * 8);
+				int totalHours =await CalculateTotalTimesheetHours(startOfNextWeek,endOfNextWeek);
 
 				weekTimesheet = new()
 				{
@@ -34,8 +30,6 @@ namespace RMT_API.Repositories
 					TotalHours = totalHours
 				};
 
-
-
 				await _context.AddAsync(weekTimesheet);
 				await _context.SaveChangesAsync();
 
@@ -45,8 +39,17 @@ namespace RMT_API.Repositories
 			return weekTimesheet;
 		}
 
+		private async Task<int> CalculateTotalTimesheetHours(DateTime startOfWeek, DateTime endOfWeek)
+		{
+			var holidaysInWeek = await _context.PublicHolidaysMaster
+						.Where(h => h.PHDate >= startOfWeek && h.PHDate <= endOfWeek && (h.IsActive ?? false))
+						.ToListAsync();
 
-		public async Task<Timesheet> GetTimesheetAsync(int resourceId, DateTime startOfWeek, DateTime endOfWeek)
+			return 40 - (holidaysInWeek.Count * 8);
+		}
+
+
+		private async Task<Timesheet> GetTimesheetAsync(int resourceId, DateTime startOfWeek, DateTime endOfWeek)
 		{
 			var _timesheet = await _context.Timesheet
 				.Where(t => t.WeekStartDate >= startOfWeek && t.WeekEndDate <= endOfWeek && (t.IsActive ?? false) && t.ResourceID == resourceId)
@@ -55,34 +58,6 @@ namespace RMT_API.Repositories
 			return _timesheet;
 
 		}
-
-		//public async Task<Timesheet> GetPreviousWeekTimesheet(int resourceId, DateTime startOfWeek)
-		//{
-		//	// Calculate the start and end dates for the previous week
-		//	DateTime startOfPrevWeek = startOfWeek==DateTime.MinValue ? GetStartOfWeek(DateTime.Now).AddDays(-7) : GetStartOfWeek(startOfWeek).AddDays(-7);
-		//	DateTime endOfPrevWeek = startOfPrevWeek.AddDays(6);
-
-		//	// Query for the timesheet for the previous week
-		//	var previousWeekTimesheet =await  _context.Timesheet
-		//		.Where(t => t.WeekStartDate >= startOfPrevWeek && t.WeekEndDate <= endOfPrevWeek && (t.IsActive ?? false) && t.ResourceID == resourceId)
-		//		.FirstOrDefaultAsync();
-
-		//	return previousWeekTimesheet;
-		//}
-
-		//public async Task<Timesheet> GetCurrentWeekTimesheet(int resourceId)
-		//{
-		//	// Calculate the start and end dates for the current week (assuming weeks start on Monday)
-		//	DateTime startOfWeek = GetStartOfWeek(DateTime.Now);
-		//	DateTime endOfWeek = startOfWeek.AddDays(6);
-
-		//	// Query for the timesheet for the current week
-		//	var currentWeekTimesheet = await _context.Timesheet
-		//								.Where(t => t.WeekStartDate >= startOfWeek && t.WeekEndDate <= endOfWeek && (t.IsActive ?? false) && t.ResourceID == resourceId)
-		//								.FirstOrDefaultAsync();
-
-		//	return currentWeekTimesheet ?? new();
-		//}
 
 		private DateTime GetStartOfWeek(DateTime date)
 		{
